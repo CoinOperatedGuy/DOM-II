dcacb71f30050d44070d17f15ced646f2f431be0
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2019-12-15 23:47:22 -0500

    second push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+let image = document.querySelector('.img');
+image.addEventListener('click', (event) => {
+    console.log("this is the image");
+});
+
+let headColor = document.querySelector('.logo-heading');
+headColor.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'blue';
+    event.target.style.color = 'white';
+});
+
+let title = document.querySelector('.destination');
+title.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'pink';
+    event.target.style.color = 'yellow';
+});
